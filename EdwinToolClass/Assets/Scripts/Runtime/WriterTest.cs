0d1572d4e0f6a0e157dd1d23af07c5144bb04832
NakXay KorYme <corby.maxime@gmail.com>
2023-10-13 22:26:10 +0200

    [ADD] : Package Google Drive + First tests

---
+++ EdwinToolClass/Assets/Scripts/Runtime/WriterTest.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor.VersionControl;
+using UnityEngine;
+
+public class WriterTest : MonoBehaviour
+{
+    public void WriteFile(byte[] fileContent)
+    {
+        string path = Path.Combine(Application.dataPath, "Test");
+        if (!File.Exists(path))
+        {
+            // Create a file to write to.
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine("Hello");
+                sw.WriteLine("And");
+                sw.WriteLine("Welcome");
+            }
+        }
+
+        // Open the file to read from.
+        //using (StreamReader sr = File.OpenText(path))
+        //{
+        //    string s;
+        //    while ((s = sr.ReadLine()) != null)
+        //    {
+        //        Console.WriteLine(s);
+        //    }
+        //}
+    }
+}
